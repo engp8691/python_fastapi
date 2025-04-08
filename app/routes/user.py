945358db5efcd2130a2fecf550a2ee5056dc9a93
Yonglin Lee <ylpro@Mac.hsd1.ma.comcast.net>
2025-04-07 21:02:43 -0400

    add crud to users end points

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,13 +1,78 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi import Path
 from app.types.user import User
 
 router = APIRouter()
+users = []
 
+# route to create a user
 @router.post("/users")
 def create_user(user: User):
-    return {"message": f"User {user.name} created", "data": user}
+    for existing_user in users:
+        if existing_user.name == user.name:
+            raise HTTPException(status_code=400, detail=f"User already exists with userid={existing_user.userid}.")
 
+    userid = 101
+    if users:
+        userid = users[-1].userid + 1 if users[-1] is not None else 101
+    else:
+        userid = 101
+
+    user.userid = userid
+    users.append(user)
+
+    return {"message": f"User {user.name} created", "user": user}
+
+# route to retrieve a user
 @router.get("/users/{user_id}")
 def get_user(user_id: int = Path(..., title="ID of the user", gt=0, description="User ID between 1 and 1000")):
-    return {"user_id": user_id}
+    try:
+        userIndex = lookForUserIndexByUserID(users, user_id)
+        return {"user": users[userIndex]}
+    except IndexError:
+        print(f"Caught an error: {IndexError}")
+        raise HTTPException(status_code=404, detail="User not found.")
+    except Exception as e:
+        raise HTTPException(status_code=500, details=f"Unexpected error : {str(e)}")    
+
+
+# route to remove a user
+@router.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    try:
+        userIndex = lookForUserIndexByUserID(users, user_id)
+        removed_user = users.pop(userIndex)
+        return {"message": "User removed", "user": removed_user}
+    except IndexError:
+        print(f"Caught an error: {IndexError}")
+        raise HTTPException(status_code=404, detail="User not found.")
+    except Exception as e:
+        raise HTTPException(status_code=500, details=f"Unexpected error : {str(e)}")
+    
+
+# route to update a user
+@router.put("/users/{user_id}")
+def update_user(user_id: int, updated_user: User):
+    try:
+        userIndex = lookForUserIndexByUserID(users, user_id)
+        updated_user.userid = user_id
+        users[userIndex] = updated_user
+
+        return {"message": "User updated", "user": updated_user}
+    except IndexError:
+        print(f"Caught an error: {IndexError}")
+        raise HTTPException(status_code=404, detail="User not found.")
+    except Exception as e:
+        raise HTTPException(status_code=500, details=f"Unexpected error : {str(e)}")
+
+def lookForUserIndexByUserID(users, user_id):
+    userFound = False
+    for index, user in enumerate(users):
+        if user and user.userid == user_id:
+            userFound = True
+            break
+    
+    if userFound:
+        return index
+    else:
+        return -1
--- app/types/user.py
+++ app/types/user.py
@@ -1,6 +1,8 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 class User(BaseModel):
     name: str = Field(..., min_length=2, max_length=50)
     age: int = Field(..., gt=0, lt=120)
     email: str
+    userid: Optional[int] = Field(default=None, gt=100, lst=1000)
