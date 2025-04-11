bfa404b4db724d9dc95cd4a1a219ae0a172363f3
yonglin li <yonglin.li@gmail.com>
2025-04-11 16:08:30 -0400

    feat: migate users table to add role property (#5)
    Co-authored-by: Yonglin Lee <ylpro@Mac.hsd1.ma.comcast.net>

---
+++ alembic/versions/9680c3d8b23d_add_role_to_users.py
@@ -0,0 +1,29 @@
+"""Add role to users
+
+Revision ID: 9680c3d8b23d
+Revises: add_hashed_password
+Create Date: 2025-04-11 15:44:59.826458
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9680c3d8b23d'
+down_revision: Union[str, None] = 'add_hashed_password'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.add_column('users',
+        sa.Column('role', sa.String(), nullable=False, server_default='')
+    )
+    op.alter_column('users', 'role', server_default=None)
+
+
+def downgrade():
+    op.drop_column('users', 'role')
