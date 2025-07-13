73c046a4dfbac5ec5e7082473f74f214e36eb3e3
Yonglin Lee <ylpro@Mac.hsd1.ma.comcast.net>
2025-07-12 22:32:45 -0400

    feat: code formatter

--- app/grpc/client.py
+++ app/grpc/client.py
@@ -3,7 +3,7 @@ from app.grpc import greeter_pb2, greeter_pb2_grpc
 
 
 def run():
-        with grpc.insecure_channel("localhost:50051") as channel:
+               with grpc.insecure_channel("localhost:50051") as channel:
     stub = greeter_pb2_grpc.GreeterStub(channel)
     response = stub.SayHello(greeter_pb2.HelloRequest(name="Yonglin"))
     print("Greeter client received: " + response.message)
